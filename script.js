32b462f2c7c9ed7ffa3717f4e877d7fae6779a4a
meenatchi <142766327+meenatchi-14@users.noreply.github.com>
2023-10-04 16:56:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const display = document.getElementById('display');
+ 
+    function appendToDisplay(value) {
+      display.value += value;
+    }
+ 
+    function clearDisplay() {
+      display.value = '';
+    }
+ 
+    function calculateResult() {
+      try {
+        display.value = eval(display.value);
+      } catch (error) {
+        display.value = 'Error';
+      }
+    }
+ 
+    document.addEventListener('keydown', function(event) {
+      const key = event.key;
+ 
+      if (/[0-9+\-*/.=]/.test(key)) {
+        appendToDisplay(key);
+      } else if (key === 'Escape') {
+        clearDisplay();
+      } else if (key === 'Enter' || key === '=') {
+        calculateResult();
+      }
+    });
